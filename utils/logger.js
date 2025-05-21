dd3a9e6960467bb68eaa0167547442d4c9b1dc1b
Atikul Islam Rabbi <atikulislamx25@gmail.com>
2025-05-21 20:01:39 +0600

    Delete utils directory

--- utils/logger.js
+++
@@ -1,7 +0,0 @@
-// File: utils/logger.js
-
-export function log(message) {
-  const time = new Date().toLocaleTimeString();
-  console.log(`[WhiteCat][${time}] ${message}`);
-  chrome.runtime.sendMessage({ type: 'log', message });
-}
