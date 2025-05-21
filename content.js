ae6ae7c62df6b25be2f0f81eddb4ac7c5aec7998
Atikul Islam Rabbi <atikulislamx25@gmail.com>
2025-05-21 17:35:23 +0600

    Create content.js

---
+++ content.js
@@ -0,0 +1,8 @@
+// File: content.js
+
+// This file can be used for interaction with Facebook page DOM,
+// but for now White Cat runs mostly in background with injected cookies.
+
+// Placeholder for potential future DOM manipulation or event listening.
+
+console.log("White Cat content script loaded.");
